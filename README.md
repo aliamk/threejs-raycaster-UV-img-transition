50963c855be3740f06c965bcfbbcb4f3694afdbc
unknown <alia.azarian@gmail.com>
2021-03-30 22:56:58 +0100

    smoothstep and image as color used

---
+++ README.md
@@ -0,0 +1,3 @@
+### Smoothstep added to the noise and 2 image files passed to the mix() function and used as color
+
+![raycast_UV_detection_imgAsColorOutput](https://user-images.githubusercontent.com/63723832/113062045-025cbf80-91ab-11eb-99fb-52e5e11fb9b7.gif)
--- js/shader/fragment.glsl
+++ js/shader/fragment.glsl
@@ -3,8 +3,24 @@ varying vec2 vUv;
 // uniform sampler2D onTexture;
 uniform sampler2D uImage;
 uniform float time;
+uniform float hoverState;
 
 void main()	{
+	vec2 newUV = vUv;
+	vec2 p = newUV;
+	float x = hoverState;
+
+	x = smoothstep(.0, 1.0, (x * 2.0 + p. y - 1.0));
+
+	vec4 imgAsColorOutput = mix(
+		texture2D(uImage, (p - .5) * (1. - x) + .5),
+		texture2D(uImage, (p - .5) * x + .5),
+	x);
+
+	vec4 onLogo = texture2D(uImage, vUv);	
+	gl_FragColor = imgAsColorOutput;
+	gl_FragColor.rgb += 0.05 * vec3(vNoise);
+
 	// gl_FragColor = vec4(1., 0., 0.0, 1.);
 	// vec2 newUV = (vUv - vec2(0.5))*resolution.zw + vec2(0.5);	
 
@@ -12,14 +28,14 @@ void main()	{
 	// vec3 color2 = vec3(1., 1., 1.);
 	// vec3 finalColor = mix(color1, color2, 0.5 * (vNoise + 1.));	
 
-	vec2 newUV = vUv;
-	vec4 onLogo = texture2D(uImage, newUV);
+	// vec2 newUV = vUv;
+	// vec4 onLogo = texture2D(uImage, newUV);
 	// newUV = vec2(newUV.x, newUV.y + 0.01 * sin(newUV.x * 10. + time));
 	// vec4 onLogo = texture2D(uImage, vUv);
 	//
 	// gl_FragColor = vec4(finalColor, 1.);
-	gl_FragColor = vec4(vUv, 0., 1.);
+	// gl_FragColor = vec4(vUv, 0., 1.);
 	// gl_FragColor = vec4(vNoise, 0., 0., 1.);
-	gl_FragColor = onLogo;
-	gl_FragColor.rgb += 0.15 * vec3(vNoise);
+	// gl_FragColor = onLogo;
+	// gl_FragColor.rgb += 0.15 * vec3(vNoise);
 }
